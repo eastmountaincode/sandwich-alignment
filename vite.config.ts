7e08050be6b43097c685e7198ef0fca113aa1545
Andrew Boylan <andrewboylan@Andrews-MacBook-Pro-M3.local>
2025-02-14 14:36:37 -0500

    change base to root

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/sandwich-alignment/',
+  base: '/',
   plugins: [
     react(),
     tailwindcss()],
