3e6c896f10225a004925fff3cabb8f3b22f863f8
Andrew Boylan <andrewboylan@Andrews-MacBook-Pro-M3.local>
2025-02-14 15:12:04 -0500

    no more storing ip

--- api/submitBoard.js
+++ api/submitBoard.js
@@ -3,23 +3,11 @@ import fetch from 'node-fetch'; // Ensure you have node-fetch installed
 
 const uri = process.env.MONGODB_URI;
 
-async function fetchIpAddress() {
-  try {
-    const response = await fetch('https://api.ipify.org?format=json');
-    const data = await response.json();
-    return data.ip;
-  } catch (error) {
-    console.error('Failed to fetch IP address:', error);
-    return 'unknown';
-  }
-}
 
 export default async function handler(req, res) {
   if (req.method === 'POST') {
     let client;
     try {
-      const ipAddress = await fetchIpAddress();
-
       // Initialize and connect the MongoDB client
       client = new MongoClient(uri);
       await client.connect();
@@ -28,7 +16,7 @@ export default async function handler(req, res) {
       const collection = db.collection('sandwich-board-records');
 
       // Insert the document
-      const result = await collection.insertOne({ ...req.body, ipAddress, createdAt: new Date() });
+      const result = await collection.insertOne({ ...req.body, createdAt: new Date() });
       res.status(200).json({ insertedId: result.insertedId });
 
     } catch (error) {
