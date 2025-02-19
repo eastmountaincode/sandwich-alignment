b96e209ca7a8e9b17f946d2a3ae99c05278af2ee
Andrew Boylan <andrewboylan@Andrews-MacBook-Pro-M3.local>
2025-02-18 23:56:07 -0500

    import sandwich data as json

--- api/generateBoard.js
+++ api/generateBoard.js
@@ -1,7 +1,7 @@
 import OpenAI from "openai";
 import { zodResponseFormat } from "openai/helpers/zod";
 import { z } from "zod";
-import sandwichData from '../src/data/sandwiches.json';
+import sandwichData from '../src/data/sandwiches.json' assert { type: 'json' };
 
 const openai = new OpenAI({
   apiKey: process.env.OPENAI_API_KEY,
